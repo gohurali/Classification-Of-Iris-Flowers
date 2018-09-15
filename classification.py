6298b7352a0168fde30f49557026f407c9caa795
gohurali <gohurali@live.com>
2018-09-15 10:29:42 -0700

    Working and learning jupyter as well...working on plotting

--- classification.py
+++ classification.py
@@ -1,19 +1,25 @@
+#%%
 import sys
 import scipy
 import numpy
+
 import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
 import pandas
 import sklearn
 
-print('Python: {}'.format(sys.version))
-print('Scipy: {}'.format(scipy.__version__))
-print('Numpy: {}'.format(numpy.__version__))
-print('MatPlotLib: {}'.format(matplotlib.__version__))
-print('sklearn: {}'.format(sklearn.__version__))
+# print('Python: {}'.format(sys.version))
+# print('Scipy: {}'.format(scipy.__version__))
+# print('Numpy: {}'.format(numpy.__version__))
+# print('MatPlotLib: {}'.format(plt.__version__))
+# print('sklearn: {}'.format(sklearn.__version__))
 
 url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 names = ['sepal-length','sepal-width','petal-length','petal-width','class']
 
 dataset = pandas.read_csv(url,names=names)
 
-print(dataset)
+dataset.plot(kind='box', subplots=True, layout=(2,2),sharex=False, sharey=False)
+plt.show()
